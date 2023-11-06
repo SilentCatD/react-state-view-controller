c0ecdc78c6a756a110b4b1b007e0de198a5aa691
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-11-06 09:22:14 +0700

    add packages file

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import Controller from './Controller'
+import {
+  ControllerProviderProps,
+  ControllerProvider,
+  BuilderProps,
+  Builder,
+  ListenerProps,
+  Listener,
+  ControllerContext,
+} from './types'
+
+import { useController } from './useController'
+import { createControllerContext } from './ControllerContext'
+
+export { Controller }
+export type {
+  ControllerProviderProps,
+  ControllerProvider,
+  BuilderProps,
+  Builder,
+  ListenerProps,
+  Listener,
+  ControllerContext,
+}
+export { useController, createControllerContext }
---
+++ src/types.ts
@@ -0,0 +1,39 @@
+import { PropsWithChildren, ReactNode } from 'react'
+import Controller from './Controller'
+
+export interface ControllerProviderProps<C extends Controller<S>, S> {
+  create: () => C
+}
+
+export type ControllerProvider<C extends Controller<S>, S> = React.FC<PropsWithChildren<ControllerProviderProps<C, S>>>
+
+export type BuilderBuildWhen<S> = (prevState: S, currentState: S) => boolean
+
+export type BuilderAction<S> = {
+  type: 'newstate'
+  payload: S
+  buildWhen?: BuilderBuildWhen<S>
+}
+
+export interface BuilderProps<S> {
+  builder: (state: S) => ReactNode
+  buildWhen?: BuilderBuildWhen<S>
+}
+
+export type Builder<S> = React.FC<BuilderProps<S>>
+
+export type ListenerListenWhen<S> = (prevState: S, currentState: S) => boolean
+
+export interface ListenerProps<S> {
+  listener: (state: S) => void
+  listenWhen?: ListenerListenWhen<S>
+}
+
+export type Listener<S> = React.FC<PropsWithChildren<ListenerProps<S>>>
+
+export interface ControllerContext<C extends Controller<S>, S> {
+  Provider: ControllerProvider<C, S>
+  Builder: Builder<S>
+  Listener: Listener<S>
+  _context: React.Context<C>
+}
