1aca18f45d869167c24badcee6904c2ed9f25394
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-11-09 13:50:41 +0700

    fix typo

--- src/useController.ts
+++ src/useController.ts
@@ -7,7 +7,7 @@ function useController<C extends Controller<S>, S>(context: ControllerContext<C,
   const controller = useContext(context._context)
   if (controller === undefined) {
     throw new ControllerNotProvided(
-      "The requested controller is not provided in scope, make sure you have wrap it's t Provider",
+      "The requested controller is not provided in scope, make sure you have wrap it's Provider",
     )
   }
   return controller
