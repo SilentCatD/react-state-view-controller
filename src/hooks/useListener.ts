745d11246404148b90016ce094bb9e7301b111b6
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-05 10:43:24 +0700

    typing for controller

--- src/hooks/useListener.ts
+++ src/hooks/useListener.ts
@@ -1,5 +1,56 @@
+import { ResourcesNotProvidedError, getObjectRuntimeName, useProvider } from 'react-scoped-provider'
 import Controller from '../Controller'
 
 type ListenerListenWhen<S> = (prevState: S, currentState: S) => boolean
 
-function useListener<C extends Controller<S>, S>(listener: (state: S) => void, listenWhen?: ListenerListenWhen<S>): C
+type InferStateType<C extends Controller<any>> = C extends Controller<infer S> ? S : never
+
+function useListener<C extends Controller<InferStateType<C>>>(
+  ctor: new (...a: any) => C,
+  listener: (state: InferStateType<C>) => void,
+  listenWhen?: ListenerListenWhen<InferStateType<C>>,
+): C
+
+function useListener<C extends Controller<InferStateType<C>>>(
+  controller: C,
+  listener: (state: InferStateType<C>) => void,
+  listenWhen?: ListenerListenWhen<InferStateType<C>>,
+): void
+
+function useListener<C extends Controller<InferStateType<C>>>(
+  source: (new (...a: any) => C) | C,
+  listener: (state: InferStateType<C>) => void,
+  listenWhen?: ListenerListenWhen<InferStateType<C>>,
+): C | undefined {
+  const controller = useResolveController(source, { allowUndef: true })
+  return controller
+}
+
+export { useListener }
+
+interface UseResolveControllerConfigs {
+  allowUndef?: boolean
+}
+
+function useResolveController<C extends Controller<InferStateType<C>>>(
+  source: (new (...a: any) => C) | C,
+  configs?: UseResolveControllerConfigs,
+): C | undefined {
+  const providedController = useProvider(
+    source instanceof Controller ? (source.constructor as new (...a: any) => C) : source,
+    {
+      allowUndef: true,
+    },
+  )
+  const passedController = source instanceof Controller ? source : undefined
+  const resolvedController = passedController ?? providedController
+  if (resolvedController === undefined) {
+    if (configs?.allowUndef === true) {
+      return undefined
+    }
+    throw new ResourcesNotProvidedError(getObjectRuntimeName(source))
+  }
+  return resolvedController
+}
+
+export { useResolveController }
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,24 @@ import { Subscription, SubscriptionLike, TeardownLogic, Unsubscribable, Observab
 import { Create } from './types'
 import { useAutoDispose } from './hooks/useAutoDispose'
 import { ControllerProvider, ControllerProviderProps } from './ControllerProvider'
-import { MultiProvider, ReceivableElement, MultiProviderProps } from 'react-scoped-provider'
+import { MultiProvider, ReceivableElement, MultiProviderProps, useProvider } from 'react-scoped-provider'
+import { useListener } from './hooks/useListener'
 
+export { useListener }
 export { MultiProvider, ReceivableElement, MultiProviderProps }
 export { ControllerProvider, ControllerProviderProps }
 export { Create }
 export { Controller }
 export { Subscription, SubscriptionLike, TeardownLogic, Unsubscribable, Observable }
 export { useAutoDispose }
+
+class TestController extends Controller<number> {
+  constructor() {
+    super(5)
+  }
+}
+
+const useTest = () => {
+  const test = new TestController()
+  const a = useListener(TestController, (state) => console.log(state))
+}
