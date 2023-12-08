9988894d8bbad44436a93d1feb6da3a646f53408
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-08 15:07:26 +0700

    fix useSelector

--- src/hooks/useSelector.ts
+++ src/hooks/useSelector.ts
@@ -3,6 +3,7 @@ import Controller from '../Controller'
 import { Constructor, InferStateType, StateCompare } from '../types'
 import { useBuilder } from './useBuilder'
 import { useControllerResolver } from './useControllerResolver'
+import { isEqual } from '../utils'
 
 function useSelector<C extends Controller<InferStateType<C>>, T>(
   ctor: Constructor<C>,
@@ -24,11 +25,21 @@ function useSelector<C extends Controller<InferStateType<C>>, T>(
   const controller = useControllerResolver(source)
   const buildWhenCallback = useCallback(
     (prev: InferStateType<C>, curr: InferStateType<C>) => {
-      return stateCompare?.(selector(prev), selector(curr)) ?? true
+      if (stateCompare === undefined) {
+        return !isEqual(selector(prev), selector(curr))
+      } else {
+        return !stateCompare(selector(prev), selector(curr))
+      }
     },
     [selector, stateCompare],
   )
-  const state = useBuilder(controller, buildWhenCallback)
+  const stateCompareCallback = useCallback(
+    (prev: InferStateType<C>, curr: InferStateType<C>) => {
+      return !buildWhenCallback(prev, curr)
+    },
+    [buildWhenCallback],
+  )
+  const state = useBuilder(controller, buildWhenCallback, stateCompareCallback)
   if (source instanceof Controller) {
     return selector(state)
   }
