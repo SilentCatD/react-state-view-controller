7ccb054ae80191c1bc6174f4756cf647c654797a
Silent Cat <ngocphuocnguyen312@gmail.com>
2023-12-08 11:00:42 +0700

    add autodispose test

---
+++ tests/hooks/use_auto_dispose.test.ts
@@ -0,0 +1,37 @@
+import { Controller, useAutoDispose } from '../../src'
+import { renderHook } from '@testing-library/react'
+
+class TestController extends Controller<number> {
+  constructor() {
+    super(0)
+  }
+}
+
+it('dispose when unmounted', () => {
+  const instance = new TestController()
+  jest.spyOn(instance, 'dispose')
+  const { unmount } = renderHook(() => useAutoDispose(() => instance))
+  unmount()
+  expect(instance.dispose).toHaveBeenCalledTimes(1)
+  jest.restoreAllMocks()
+})
+
+it('create resources', () => {
+  const mockCreate = jest.fn()
+  mockCreate.mockReturnValueOnce(new TestController())
+  const { result } = renderHook(() => useAutoDispose(() => mockCreate()))
+  const controller = result.current
+  expect(controller).toBeInstanceOf(TestController)
+  expect(mockCreate.mock.calls.length).toBe(1)
+})
+
+it('resouces is the same', () => {
+  const instance = new TestController()
+  const mockCreate = jest.fn()
+  mockCreate.mockReturnValueOnce(instance)
+  const { result } = renderHook(() => useAutoDispose(() => mockCreate()))
+  const controller = result.current
+  expect(controller).toBeInstanceOf(TestController)
+  expect(controller).toBe(instance)
+  expect(mockCreate.mock.calls.length).toBe(1)
+})
